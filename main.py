0e693defff4b798f75f585c72afac99fa56de346
Abdul Hannan Kanji <hannanabdul55@gmail.com>
2021-02-08 15:49:11 -0500

    Add code comments and change some labels

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ from code.validation_functions.forecast_filename import validate_forecast_file_n
 from code.validation_functions.forecast_date import filename_match_forecast_date
 from code.test_formatting import forecast_check, validate_forecast_file, print_output_errors
 
+# Pattern that matches a forecast file add to the data-processed folder.
+# Test this regex usiing this link: https://regex101.com/r/f0bSR3/1 
 pat = re.compile(r"^data-processed/(.+)/\d\d\d\d-\d\d-\d\d-\1\.csv$")
 
-forecasts = []
 
 local = os.environ.get('CI') != 'true'
 # local = True
@@ -38,42 +39,58 @@ if not local:
 else:
     event = json.load(open("test/test_event.json"))
 
-# print(f"Event: {event}")
+
 pr = None
 comment = ''
+files_changed = []
+
 if os.environ.get('GITHUB_EVENT_NAME') == 'pull_request_target' or local:
-    # pr_num = int(os.environ.get('GITHUB_REF').split('/')[-2])
+    # Fetch the  PR number from the event json
     pr_num = event['pull_request']['number']
     print(f"PR number: {pr_num}")
+
+    # Use the Github API to fetch the Pullrequest Object. Refer to details here: https://pygithub.readthedocs.io/en/latest/github_objects/PullRequest.html 
+    # pr is the Pullrequest object
     pr = repo.get_pull(pr_num)
-    forecasts +=[f for f in pr.get_files()]
-forecasts = [file for file in forecasts if pat.match(file.filename) is not None]
-other_files = [file for file in forecasts if pat.match(file.filename) is None]
+
+    # fetch all files changed in this PR and add it to the files_changed list.
+    files_changed +=[f for f in pr.get_files()]
+
+# Split all files in `files_changed` list into valid forecasts and other files
+forecasts = [file for file in files_changed if pat.match(file.filename) is not None]
+other_files = [file for file in files_changed if pat.match(file.filename) is None]
 
 if os.environ.get('GITHUB_EVENT_NAME') == 'pull_request_target':
-    if len(other_files) > 0:
+    # IF there are other fiels changed in the PR 
+    #TODO: If there are other files changed as well as forecast files added, then add a comment saying so. 
+    if len(other_files) > 0 and len(forecasts) >0:
         print(f"PR has other files changed too.")
-    else:
-        if pr is not None and not local:
-            pr.add_to_labels('data-submission')
+        if pr is not None:
+            pr.add_to_labels('other-files-updated')
+    # Do not require this as it is done by the PR labeler action.
+    # else:
+    #     if pr is not None:
+    #         pr.add_to_labels('data-submission')
 
     deleted_forecasts = False
+    
+    # `f` is ab object of type: https://pygithub.readthedocs.io/en/latest/github_objects/File.html 
+    # `forecasts` is a list of `File`s that are changed in the PR.
     for f in forecasts:
+        # TODO: Add a better way of checking whether a file is deleted or not. Currently, this checks if there are ANY deletion in a forecast file.
         if f.deletions >0:
             deleted_forecasts = True
-    if deleted_forecasts and not local:
+    if deleted_forecasts:
+        # Add the `forecast-updated` label when there are deletions in the forecast file
         pr.add_to_labels('forecast-updated')
         comment += "\n Your submission seem to have updated/deleted some forecasts. Could you provide a reason for the updation/deletion? Thank you!\n\n"
 
-
-
-# print(f"Forecasts updated/added: {forecasts}")
 
 # Download all forecasts
 # create a forecasts directory
 os.makedirs('forecasts', exist_ok=True)
 
-# Download all forecasts into the forecasts folder
+# Download all forecasts changed in the PR into the forecasts folder
 for f in forecasts:
     urllib.request.urlretrieve(f.raw_url, f"forecasts/{f.filename.split('/')[-1]}")
 
@@ -85,7 +102,7 @@ for file in glob.glob("forecasts/*.csv"):
     if len(error_file) >0:
         errors[os.path.basename(file)] = error_file
     
-    # Check for the forecast date column check
+    # Check for the forecast date column check is +-1 day from the current date the PR build is running
     is_err, err_message = filename_match_forecast_date(file)
     if is_err:
         comment+= err_message
