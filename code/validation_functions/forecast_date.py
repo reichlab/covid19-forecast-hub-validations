0883805c3bffbeacb364a928cde6078374f133e7
Abdul Hannan Kanji <hannanabdul55@gmail.com>
2021-02-14 16:25:31 -0500

    Fix return value on forecast date error.

--- code/validation_functions/forecast_date.py
+++ code/validation_functions/forecast_date.py
@@ -9,13 +9,13 @@ def filename_match_forecast_date(filepath):
     file_forecast_date = os.path.basename(os.path.basename(filepath))[:10]
     forecast_date_column = set(list(df['forecast_date']))
     if len(forecast_date_column) > 1:
-        return True, ["FORECAST DATE ERROR: %s has multiple forecast dates: %s. Forecast date must be unique" % (
-            filepath, forecast_date_column)]
+        return True, "FORECAST DATE ERROR: %s has multiple forecast dates: %s. Forecast date must be unique" % (
+            filepath, forecast_date_column)
     else:
         forecast_date_column = forecast_date_column.pop()
         if (file_forecast_date != forecast_date_column):
-            return True, ["FORECAST DATE ERROR %s forecast filename date %s does match forecast_date column %s" % (
-                filepath, file_forecast_date, forecast_date_column)]
+            return True, "FORECAST DATE ERROR %s forecast filename date %s does match forecast_date column %s" % (
+                filepath, file_forecast_date, forecast_date_column)
         today = datetime.now(pytz.timezone('US/Eastern')).date()
         forecast_date = datetime.strptime(file_forecast_date, "%Y-%m-%d").date()
         if abs(forecast_date.day - today.day) >1 or forecast_date.month != today.month or forecast_date.year != today.year:
