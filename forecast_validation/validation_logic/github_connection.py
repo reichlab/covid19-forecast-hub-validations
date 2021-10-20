b13d8915745fd744a23c62b2bf6cb0ff79203cd2
Yuxin Huang <yuxinhuang@umass.edu>
2021-10-20 14:21:13 -0400

    [WIP] add label

--- forecast_validation/validation_logic/github_connection.py
+++ forecast_validation/validation_logic/github_connection.py
@@ -133,6 +133,7 @@ def determine_pull_request_type(store: dict[str, Any]) -> ValidationStepResult:
     if not is_forecast_submission(filtered_files):
         other_files = filtered_files[PullRequestFileType.OTHER_NONFS]
         for file in other_files:
+            labels.add(all_labels["other-files-updated"])
             if file.filename.startswith("code"):
                 labels.add(all_labels["code"])
             if os.path.basename(file.filename) == "package.json":
