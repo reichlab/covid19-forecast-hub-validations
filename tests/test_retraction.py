97044f34d3be38a0a2ab124b488076c435e6e038
vidhi-mody <vidhimody6@gmail.com>
2021-11-12 12:51:17 -0500

    add: unit tests

--- tests/test_retraction.py
+++ tests/test_retraction.py
@@ -1,48 +1,55 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import unittest
 from forecast_validation.checks.forecast_file_content import compare_forecasts
 
 # List of sample PRs that we want to check against
 test_prs = [3448, 3342]
+
+
 class ValidationRetractionTest(unittest.TestCase):
     def test_updation_with_no_new_targets(self):
         self.assertFalse(
             compare_forecasts(
-                "tests/testfiles/old_forecast.csv",
-                "tests/testfiles/new_forecast.csv"
+                "tests/testfiles/data-processed/teamA-modelA/forecast_content-original_forecast.csv",
+                "tests/testfiles/data-processed/teamA-modelA/forecast_content-value_update.csv",
             ).has_implicit_retraction
         )
 
     def test_updation_with_new_targets(self):
         self.assertFalse(
             compare_forecasts(
-                "tests/testfiles/old_forecast.csv",
-                "tests/testfiles/new_forecast_new_targets.csv"
+                "tests/testfiles/data-processed/teamA-modelA/forecast_content-original_forecast.csv",
+                "tests/testfiles/data-processed/teamA-modelA/forecast_content-new_targets.csv"
             ).has_implicit_retraction
         )
+        # need to check forecast update boolean
 
     def test_updation_with_removed_targets(self):
         self.assertTrue(
             compare_forecasts(
-                "tests/testfiles/old_forecast.csv",
-                "tests/testfiles/new_forecast_removed_targets.csv"
+                "tests/testfiles/data-processed/teamA-modelA/forecast_content-original_forecast.csv",
+                "tests/testfiles/data-processed/teamA-modelA/forecast_content-implicit_retractions.csv"
             ).has_implicit_retraction
         )
 
     def test_updation_with_all_duplicates(self):
         self.assertTrue(
             compare_forecasts(
-                "tests/testfiles/old_forecast.csv",
-                "tests/testfiles/new_forecast_same_rows.csv"
+                "tests/testfiles/data-processed/teamA-modelA/forecast_content-original_forecast.csv",
+                "tests/testfiles/data-processed/teamA-modelA/forecast_content-duplicate.csv"
             ).is_all_duplicate
         )
-    
+
     def test_updation_with_explicit_retractions(self):
         self.assertTrue(
             compare_forecasts(
-                "tests/testfiles/old_forecast.csv",
-                "tests/testfiles/new_forecast_explicit_retractions.csv"
+                "tests/testfiles/data-processed/teamA-modelA/forecast_content-original_forecast.csv",
+                "tests/testfiles/data-processed/teamA-modelA/forecast_content-explicit_retractions.csv"
             ).has_explicit_retraction
         )
+
 
 if __name__ == '__main__':
     unittest.main()
