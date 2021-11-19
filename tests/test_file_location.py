763fd293fd6f801a0d3a820a57be8a048423a4c3
vidhi-mody <vidhimody6@gmail.com>
2021-11-18 19:29:33 -0500

    use: mock functions

--- tests/test_file_location.py
+++ tests/test_file_location.py
@@ -1,4 +1,3 @@
-import re
 import unittest
 import sys
 import os
@@ -7,14 +6,13 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from forecast_validation import FILENAME_PATTERNS
 from forecast_validation import PullRequestFileType
 from forecast_validation.checks.forecast_file_type import filter_files
-
+from unittest.mock import MagicMock
 
 class ValidationFileLocationTest(unittest.TestCase):
 
     #A file identified as a forecast is submitted in the correct model folder.
     def test_valid_forecast_location(self):
-        forecast_file = FakeGithubFile(
-            "data-processed/teamA-modelA/2021-11-08-teamA-modelA.csv")
+        forecast_file = MagicMock(filename =  "data-processed/teamA-modelA/2021-11-08-teamA-modelA.csv")
         actual = filter_files([forecast_file], FILENAME_PATTERNS)
         self.assertTrue(actual.get(PullRequestFileType.FORECAST), forecast_file)
         self.assertFalse(actual.get(PullRequestFileType.OTHER_FS), forecast_file)
@@ -23,8 +21,7 @@ class ValidationFileLocationTest(unittest.TestCase):
 
     # A file identified as a forecast is submitted in data-processed but not model folder
     def test_invalid_forecast_location(self):
-        forecast_file = FakeGithubFile(
-            "data-processed/2021-11-08-teamA-modelA.csv")
+        forecast_file = MagicMock(filename =  "data-processed/2021-11-08-teamA-modelA.csv")
         actual = filter_files([forecast_file], FILENAME_PATTERNS)
         self.assertFalse(actual.get(PullRequestFileType.FORECAST),forecast_file)
         self.assertTrue(actual.get(PullRequestFileType.OTHER_FS), forecast_file)
@@ -33,7 +30,7 @@ class ValidationFileLocationTest(unittest.TestCase):
 
     # A file identified as a forecast is submitted but it updates files outside the data-processed folder
     def test_invalid_forecast_location_root(self):
-        forecast_file = FakeGithubFile("2021-11-08-teamA-modelA.csv")
+        forecast_file = MagicMock(filename =  "2021-11-08-teamA-modelA.csv")
         actual = filter_files([forecast_file], FILENAME_PATTERNS)
         self.assertFalse(actual.get(PullRequestFileType.OTHER_FS), forecast_file)
         self.assertTrue(actual.get(PullRequestFileType.OTHER_NONFS), forecast_file)
@@ -42,8 +39,7 @@ class ValidationFileLocationTest(unittest.TestCase):
     
     #Correct name 
     def test_valid_file_name(self):
-        forecast_file = FakeGithubFile(
-            "data-processed/teamA-modelA/2021-11-08-teamA-modelA.csv")
+        forecast_file = MagicMock(filename =  "data-processed/teamA-modelA/2021-11-08-teamA-modelA.csv")
         actual = filter_files([forecast_file], FILENAME_PATTERNS)
         self.assertTrue(actual.get(PullRequestFileType.FORECAST), forecast_file)
         self.assertFalse(actual.get(PullRequestFileType.OTHER_FS), forecast_file)
@@ -52,8 +48,7 @@ class ValidationFileLocationTest(unittest.TestCase):
 
     #Incorrect name
     def test_valid_infile_name(self):
-        forecast_file = FakeGithubFile(
-            "data-processed/teamA-modelA/ABC-2021-11-08-modelA.csv")
+        forecast_file = MagicMock(filename =  "data-processed/teamA-modelA/ABC-2021-11-08-modelA.csv")
         actual = filter_files([forecast_file], FILENAME_PATTERNS)
         self.assertFalse(actual.get(PullRequestFileType.FORECAST), forecast_file)
         self.assertTrue(actual.get(PullRequestFileType.OTHER_FS), forecast_file)
@@ -62,8 +57,7 @@ class ValidationFileLocationTest(unittest.TestCase):
 
     #A file identified as metadata file submitted in the correct model folder
     def test_valid_metadata_file(self):
-        forecast_file = FakeGithubFile(
-            "data-processed/teamA-modelA/metadata-teamA-modelA.txt")
+        forecast_file = MagicMock(filename =  "data-processed/teamA-modelA/metadata-teamA-modelA.txt")
         actual = filter_files([forecast_file], FILENAME_PATTERNS)
         self.assertFalse(actual.get(PullRequestFileType.FORECAST), forecast_file)
         self.assertFalse(actual.get(PullRequestFileType.OTHER_FS), forecast_file)
@@ -72,19 +66,12 @@ class ValidationFileLocationTest(unittest.TestCase):
 
     #A file identified as metadata file submitted in incorrect model folder
     def test_invalid_metadata_file(self):
-        forecast_file = FakeGithubFile(
-            "data-processed/teamB-modelB/metadata-teamA-modelA.txt")
+        forecast_file = MagicMock(filename =  "data-processed/teamB-modelB/metadata-teamA-modelA.txt")
         actual = filter_files([forecast_file], FILENAME_PATTERNS)
         self.assertFalse(actual.get(PullRequestFileType.FORECAST), forecast_file)
         self.assertTrue(actual.get(PullRequestFileType.OTHER_FS), forecast_file)
         self.assertFalse(actual.get(PullRequestFileType.OTHER_NONFS), forecast_file)
         self.assertFalse(actual.get(PullRequestFileType.METADATA), forecast_file)
-
-
-class FakeGithubFile():
-    def __init__(self, filenane):
-        self.filename = filenane
-
 
 if __name__ == '__main__':
     unittest.main()
