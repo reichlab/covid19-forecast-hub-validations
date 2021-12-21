4cf5b1aaa98d02c8bb34138650d035f4956e2575
Vidhi Mody <vidhimody6@gmail.com>
2021-12-21 12:40:07 -0500

    Update test_metadata_content.py

--- tests/test_metadata_content.py
+++ tests/test_metadata_content.py
@@ -11,14 +11,12 @@ class ValidationMetadataTest(unittest.TestCase):
     #A valid covid hub metadata file is submitted
     def test_covid_hub_valid_metadata_content(self):
         is_invalid, errors = check_metadata_file(Path("tests/testfiles/data-processed/teamF-modelF/metadata-teamF-modelF.txt"))
-        #print(is_invalid, errors)
         self.assertFalse(is_invalid)
         self.assertEqual(errors, "no errors")
 
     #A valid flu hub metadata file is submitted
     def test_flu_valid_metadata_content(self):
         is_invalid, errors = check_metadata_file(Path("tests/testfiles/data-forecasts/teamA-modelA/metadata-teamA-modelA.txt"))
-        #print(is_invalid, errors)
         self.assertFalse(is_invalid)
         self.assertEqual(errors, "no errors")
     
@@ -26,14 +24,12 @@ class ValidationMetadataTest(unittest.TestCase):
     #A covid hub metadata file is submitted in incorrect model folder
     def test_invalid_metadata_folder(self):
         is_invalid, errors = check_metadata_file(Path("tests/testfiles/data-processed/teamD-modelD/metadata-teamD-modelA.txt"))
-        #print(is_invalid, errors)
         self.assertTrue(is_invalid)
         self.assertIn("METADATA_ERROR: Model abreviation in metadata inconsistent with folder name", errors[0])
 
     #A flu hub metadata file is submitted in incorrect model folder
     def test_invalid_metadata_folder(self):
         is_invalid, errors = check_metadata_file(Path("tests/testfiles/data-forecasts/teamD-modelD/metadata-teamD-modelA.txt"))
-        #print(is_invalid, errors)
         self.assertTrue(is_invalid)
         self.assertIn("METADATA_ERROR: Model abreviation in metadata inconsistent with folder name", errors[0])
 
@@ -41,14 +37,12 @@ class ValidationMetadataTest(unittest.TestCase):
     # covid hub Metadata is not a valid yaml file
     def test_invalid_metadata_yaml_file(self):
         is_invalid, errors = check_metadata_file(Path("tests/testfiles/data-processed/teamA-modelA/metadata-teamA-modelC.txt"))
-        #print(is_invalid, errors)
         self.assertTrue(is_invalid)
         self.assertIn("METADATA ERROR: Metadata YAML Format Error", errors[0])
 
     # flu hub Metadata is not a valid yaml file
     def test_invalid_metadata_yaml_file(self):
         is_invalid, errors = check_metadata_file(Path("tests/testfiles/data-forecasts/teamA-modelA/metadata-teamA-modelC.txt"))
-        #print(is_invalid, errors)
         self.assertTrue(is_invalid)
         self.assertIn("METADATA ERROR: Metadata YAML Format Error", errors[0])
 
@@ -56,14 +50,12 @@ class ValidationMetadataTest(unittest.TestCase):
     #A covid hub metadata file is submitted with special characters and extra space in the end of the file
     def test_invalid_metadata_file_format(self):
         is_invalid, errors = check_metadata_file(Path("tests/testfiles/data-processed/teamE-modelE/metadata-teamE-modelE.txt"))
-        #print(is_invalid, errors)
         self.assertTrue(is_invalid)
         self.assertIn("METADATA ERROR: Metadata YAML Format Error", errors[0])
 
     #A flu hub metadata file is submitted with special characters and extra space in the end of the file
     def test_invalid_metadata_file_format(self):
         is_invalid, errors = check_metadata_file(Path("tests/testfiles/data-forecasts/teamE-modelE/metadata-teamE-modelE.txt"))
-        #print(is_invalid, errors)
         self.assertTrue(is_invalid)
         self.assertIn("METADATA ERROR: Metadata YAML Format Error", errors[0])
         
@@ -71,14 +63,12 @@ class ValidationMetadataTest(unittest.TestCase):
     #A covid hub metadata file without 'model_abbr' is submitted
     def test_missing_model_abbr(self):
         is_invalid, errors = check_metadata_file(Path("tests/testfiles/data-processed/teamB-modelB/metadata-teamB-modelB.txt"))
-        #print(is_invalid, errors)
         self.assertTrue(is_invalid)
         self.assertIn("METADATA_ERROR: Cannot find required key 'model_abbr'.", errors[0])
 
     #A flu hub metadata file without 'model_abbr' is submitted
     def test_missing_model_abbr(self):
         is_invalid, errors = check_metadata_file(Path("tests/testfiles/data-forecasts/teamB-modelB/metadata-teamB-modelB.txt"))
-        #print(is_invalid, errors)
         self.assertTrue(is_invalid)
         self.assertIn("METADATA_ERROR: Cannot find required key 'model_abbr'.", errors[0])
 
@@ -86,14 +76,12 @@ class ValidationMetadataTest(unittest.TestCase):
     #A valid covid hub metadata file with incorrect/inconsistent model_abbr is submitted
     def test_inconsistent_model_abbr(self):
         is_invalid, errors = check_metadata_file(Path("tests/testfiles/data-processed/teamB-modelB/metadata-teamA-modelA.txt"))
-        #print(is_invalid, errors)
         self.assertTrue(is_invalid)
         self.assertIn("METADATA_ERROR: Model abreviation in metadata inconsistent with folder name for model_abbr", errors[0])
 
     #A valid flu hub metadata file with incorrect/inconsistent model_abbr is submitted
     def test_inconsistent_model_abbr(self):
         is_invalid, errors = check_metadata_file(Path("tests/testfiles/data-forecasts/teamB-modelB/metadata-teamA-modelA.txt"))
-        #print(is_invalid, errors)
         self.assertTrue(is_invalid)
         self.assertIn("METADATA_ERROR: Model abreviation in metadata inconsistent with folder name for model_abbr", errors[0])
 
@@ -101,14 +89,12 @@ class ValidationMetadataTest(unittest.TestCase):
     #A valid covid hub metadata file with invalid license is submitted
     def test_invalid_license(self):
         is_invalid, errors = check_metadata_file(Path("tests/testfiles/data-processed/teamC-modelC/metadata-teamC-modelC.txt"))
-        #print(is_invalid, errors)
         self.assertTrue(is_invalid)
         self.assertIn("'license' field must be in `./code/accepted-licenses.csv`", errors[0])
 
     #A valid flu hub metadata file with invalid license is submitted
     def test_invalid_license(self):
         is_invalid, errors = check_metadata_file(Path("tests/testfiles/data-forecasts/teamC-modelC/metadata-teamC-modelC.txt"))
-        #print(is_invalid, errors)
         self.assertTrue(is_invalid)
         self.assertIn("'license' field must be in `./code/accepted-licenses.csv`", errors[0])
 
